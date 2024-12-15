1801b4ddc7c5fd68e99bb574b7e303dcac8e008f
windowslee <slee12463@gmail.com>
2024-12-15 15:38:54 -0500

    update

--- src/components/Gordo.jsx
+++ src/components/Gordo.jsx
@@ -6,7 +6,7 @@ const Gordo = () => {
     
       
       
-      <div className="h-screen flex justify-center items-center md:mt-28">
+      <div className="h-screen flex justify-center items-center mt-10 mb-10 md:mt-28">
         <div>
           <img src={gordologo} alt="" className=" " />
         </div>
